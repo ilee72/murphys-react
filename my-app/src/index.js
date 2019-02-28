22b5ae5f753e8986d84824f702bdbf3c8a837720
ilee72 <46728285+ilee72@users.noreply.github.com>
2019-02-27 18:51:40 -1000

    idk

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Menu, Dropdown, Icon, Image, Grid, GridColumn, List, Input, Button } from 'semantic-ui-react';
 
 class Murphys extends React.Component {
-
   render() {
     return (
-        <Container textAlign="center">
-    <Header as='h1'>Murphys</Header>
-        </Container>
-  );
+        <Menu borderless className="topmenu">
+          <Container>
+            <Image left src='http://murphyshawaii.com/media/2014/04/murphyshawaii.png'/>
+            <Menu.Item position="right"><Icon name="Home"/></Menu.Item>
+            <Dropdown item text="About Us">
+              <Dropdown.Menu>
+                <Dropdown.Item>Getting Here/Parking</Dropdown.Item>
+                <Dropdown.Item>Catering/Private Events</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Menu.Item fitted><item text="St. Patrick's Day"/></Menu.Item>
+            <Menu.Item fitted><Icon name="user"/></Menu.Item>
+            <Dropdown item text="MY CART 0" icon="shop">
+              <Dropdown.Menu>
+                <Dropdown.Item>My cart is currently empty.</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
+    )
   }
 }
-
 ReactDOM.render(<Murphys/>, document.getElementById('root'));
